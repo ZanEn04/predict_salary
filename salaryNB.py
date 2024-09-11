21bbdc4015561312125d6d54ff85679a85499237
Orange. <129045617+ZanEn04@users.noreply.github.com>
2024-09-11 21:12:46 +0800

    Update salaryNB.py

--- salaryNB.py
+++ salaryNB.py
@@ -109,7 +109,11 @@ def main():
     
             # Predict using the trained model
             prediction = model.predict(final_input_data_scaled)
-            predicted_salary = '>50K' if prediction[0] == 1 else '<=50K'
+            # predicted_salary = '>50K' if prediction[0] == 1 elif prediction[1] == 1 '<=50K' else '0'
+            if prediction[0] == 1:
+                predicted_salary = '>50K'
+            else:
+                predicted_salary = '<=50K'
     
             # Display prediction
             st.success(f'The predicted salary for the provided details is: {predicted_salary}')
